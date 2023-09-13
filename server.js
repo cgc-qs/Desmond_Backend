5777ea2a6ea8abb71aad8a101d8f9945b0b6513e
cgc-qs <87303775+cgc-qs@users.noreply.github.com>
2023-09-13 15:12:58 +0000

    corsOption

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const alert = require("./alert");
 const app = express();
 
 var corsOptions = {
-  origin: ['http://localhost:8081', 'http://localhost:3000'],
+  origin: ['http://0.0.0.0:8081', 'http://0.0.0.0:3000'],
   methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
   // allowedHeaders: [
   //   'Content-Type',
